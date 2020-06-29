63023ea26f6f366ef72a89c7917b20c1c228612f
Simon <s.lechner.biz@gmail.com>
2020-06-29 11:56:57 +0200

    cleanup

--- src/main/java/com/codersbay/gerhofer/Square.java
+++ src/main/java/com/codersbay/gerhofer/Square.java
@@ -4,13 +4,12 @@ public class Square extends Rectangle {
     public double length;
     public double width;
 
-    public Square(double length, double width) {
-        super(length, width);
+    public Square(double length) {
+        super(length, length);
         if (length <= 0 || width <= 0) {
             throw new IllegalArgumentException("Side length can't be zero or negative");
         }
         this.length = length;
         this.width = length;
     }
-
 }
